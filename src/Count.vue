bb7b50e29c4a5f617d49fdc781229878ca51f269
AppleKevin1106 <applekevin10141106@gmail.com>
2023-03-29 04:31:51 -0400

    init commit

---
+++ src/Count.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+export default {
+    data() {
+        return {
+            count: 0
+        }
+    },
+
+    methods: {
+        increment() {
+            console.log("increment", this.count++);
+
+            // this.count++
+        }
+    },
+
+    mounted() {
+        console.log(`The initial count is ${this.count}`);
+    }
+}
+
+
+// import { ref, onMounted } from 'vue';
+// const count = ref(0);
+// console.log("instance created");
+// function increment() {
+//     count.value++
+// }
+
+</script>
+
+<template>
+    <button @click="increment"> Count is : {{ count }}</button>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp, render } from 'vue'
+import App from './App.vue'
+import Count from './Count.vue'
+import './assets/main.css'
+
+createApp(Count).mount('#app')
